d088658cb41f7cc06bd7371624aae9826f26a2a9
Precious <bathabileprecious84@gmail.com>
2023-07-13 14:23:50 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 
 /**
  * main - prints "Programming is like building a multilingual
- * puzzle, followed by a new line.
+ * puzzle, followed by a newline.
  * Return: Always 0.
  */
 
@@ -11,6 +11,5 @@ int main(void)
 
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 }	
