c36f237ed271da73172cde19817b8b1f030df785
Precious <bathabileprecious84@gmail.com>
2023-07-27 03:12:46 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -20,10 +20,10 @@ char *_strncpy(char *dest, char *src, int n)
 	src_len++;
 
 	for (index = 0; src[index] && index < n; index++)
-	dest[index]= src[index];
+	dest[index] = src[index];
 
 	for (index = src_len; index < n; index++)
-	dest [index] = '\0';
+	dest[index] = '\0';
 
 	return (dest);
 }
