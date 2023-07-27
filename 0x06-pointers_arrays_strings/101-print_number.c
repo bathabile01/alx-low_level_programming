11b19458eeef3b98203de22fad7e9b101ea1e6ee
Precious <bathabileprecious84@gmail.com>
2023-07-27 10:49:57 -0700

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_number - Print numbers chars
+ * @n: Inteeger params
+ * Return: 0
+ */
+
+void print_number(int n)
+
+{
+	unsigned int ni;
+
+	ni = n;
+
+	if (n < 0)
+	{
+	_putchar('-');
+	ni = -n;
+	}
+
+	if (ni / 10 != 0)
+	{
+	print_number(ni / 10);
+	}
+	_putchar((ni % 10) + '0');
+}
