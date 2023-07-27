e8144242ea6a4781def60fbc6f2d926ecbe55ea5
Precious <bathabileprecious84@gmail.com>
2023-07-27 12:29:15 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -4,7 +4,7 @@ int main(void)
 
 {
 	int n;
-	int =[5];
+	int a[5];
 	int *p;
 
 	a[2] - 1024;
@@ -21,5 +21,4 @@ int main(void)
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2];
 	return (0);
-	
-	}	
+	}
