df300cf290322aea055cf00c27a7b7d3936bf3f5
bathabile01 <bathabileprecious84@gmail.com>
2023-08-24 10:48:53 -0700

    4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,10 +2,9 @@
 #include "lists.h"
 
 /**
- * free_list - Frees a linked list
+ * free_list - frees a linked list
  * @head: list_t list to be freed
  */
-
 void free_list(list_t *head)
 {
 	list_t *temp;
